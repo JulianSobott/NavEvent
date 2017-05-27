64bd33e1ed1c72dcdf98ff75c25120546b10446d
Erik Götzfried <erikgoe@users.noreply.github.com>
2017-05-27 17:53:04 +0200

    Added QrCodeReaderLogic

--- NavEvent/app/src/main/java/com/unknown/navevent/interfaces/QrCodeReaderLogicInterface.java
+++ NavEvent/app/src/main/java/com/unknown/navevent/interfaces/QrCodeReaderLogicInterface.java
@@ -1,8 +1,15 @@
 package com.unknown.navevent.interfaces;
 
+import android.content.Context;
+
 //Interface for the QrCodeReader-Logic
 public interface QrCodeReaderLogicInterface
 {
+	//Should be called on creation.
+	void onCreate(Context context);
+	//Should be called on destruction.
+	void onDestroy();
+
 	//Will start camera, take a picture and generate the map-id/-link. Will respond with QrCodeReaderUI.capturedMapID().
 	void takePicture();
 }
