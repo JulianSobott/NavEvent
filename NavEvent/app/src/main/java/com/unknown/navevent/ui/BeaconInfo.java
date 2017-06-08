16d6b8be0e85c6829e00a4b316abbc294a1c6c32
Kai W <kai.w5b@web.de>
2017-06-08 14:25:29 +0200

    Same as the last one now it actually works

--- NavEvent/app/src/main/java/com/unknown/navevent/ui/BeaconInfo.java
+++ NavEvent/app/src/main/java/com/unknown/navevent/ui/BeaconInfo.java
@@ -1,37 +1,37 @@
 package com.unknown.navevent.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.Activity;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.EditText;
 
 import com.unknown.navevent.R;
+import com.unknown.navevent.bLogic.BottomSheetLogic;
+import com.unknown.navevent.interfaces.BottomSheetLogicInterface;
 import com.unknown.navevent.interfaces.BottomSheetUI;
 
 public class BeaconInfo extends Fragment implements BottomSheetUI{
 	private TextView infoText;
+	private BottomSheetLogicInterface mIfc = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_beacon_info, container, false);
 		infoText = (TextView) v.findViewById(R.id.textView);
-		return v;
+        mIfc=new BottomSheetLogic(this);
+        return v;
 	}
 
 	public void changeText(String text) {
 		infoText.setText(text);
 	}
+
+    public void updateBeaconText(int idNearestBeacon){
+        mIfc.getBeaconInfo(idNearestBeacon);
+    }
 
 	@Override
 	public void beaconInfoRespond(String info) {
