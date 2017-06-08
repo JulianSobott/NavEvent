f63f940bd7f810d4b211d558cac5e539902047fd
Kai W <kai.w5b@web.de>
2017-06-08 14:58:13 +0200

    added the activity that harbours the QRscanner

---
+++ NavEvent/app/src/main/java/com/unknown/navevent/ui/QrActivity.java
@@ -0,0 +1,15 @@
+package com.unknown.navevent.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.unknown.navevent.R;
+
+public class QrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+    }
+}
