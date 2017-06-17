a05479f424b16f77d7c88c769c048cc92b3cad13
Kai W <kai.w5b@web.de>
2017-06-17 11:55:04 +0200

    Deleted useless code

--- NavEvent/app/src/main/java/com/unknown/navevent/ui/MapDisplayFragment.java
+++ NavEvent/app/src/main/java/com/unknown/navevent/ui/MapDisplayFragment.java
@@ -11,7 +11,6 @@ import com.unknown.navevent.R;
 
 public class MapDisplayFragment extends Fragment {
 
-	View v;
 	DrawTheMap theMap;
 
 	@Override
@@ -22,24 +21,11 @@ public class MapDisplayFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		v = inflater.inflate(R.layout.fragment_map_display, container, false);
-		//if(theMap!=null){
 		theMap = new DrawTheMap(getActivity(), MainActivity.getMap());
 		return theMap;
 	}
 
 	public void LoadBeacons() {
 		theMap.loadMap(MainActivity.getMap());
-
 	}
-
-	static public int getBeaconToDisplay(MapDataForUI map) {
-		// // TODO Add a method to return the Beacon that is closest to you because it is always seleced an dits massage is displayed at the bottom by default
-
-		return 0;
-	}
-
-    /*public int getParentsViewID(){
-        return ((ViewGroup) getView().getParent()).getId();
-    }*/
 }
