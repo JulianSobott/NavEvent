e39ee058cd30119106250fe21641b4f3479b8599
Erik Götzfried <erikgoe@users.noreply.github.com>
2017-06-27 20:06:34 +0200

    Removed Option panel from bLogic

--- NavEvent/app/src/main/java/com/unknown/navevent/interfaces/OptionActivityUI.java
+++
@@ -1,9 +0,0 @@
-package com.unknown.navevent.interfaces;
-
-import java.util.Map;
-
-public interface OptionActivityUI {
-
-	//The current settings in name-value pairs.
-	void currentSettings(Map<String, String> values);
-}
